b3a654559ecb151f02be31cc3250782ac365f545
marciogama <marciogama2000@gmail.com>
2020-11-19 10:07:14 -0300

    Adicionando categorias ao formulário de cadastro

--- src/App.js
+++ src/App.js
@@ -3,48 +3,51 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
-import './assets/index.css';
+import "./assets/index.css";
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      notas:[],
-      categorias: []
-    }
+      notas: [],
+      categorias: [],
+    };
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
-  }
-
-  adicionarCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
-    const novoEstado = {...this.state, categorias:novoArrayCategorias};
+      notas: novoArrayNotas,
+    };
     this.setState(novoEstado);
   }
 
-  deletarNota(index){
-    let arrayNotas = this.state.notas;  // atribuo a essa variavel o array de notas
-    arrayNotas.splice(index,1);
-    this.setState({notas:arrayNotas});
+  adicionarCategoria(nomeCategoria) {
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+    const novoEstado = { ...this.state, categorias: novoArrayCategorias };
+    this.setState(novoEstado);
+  }
+
+  deletarNota(index) {
+    let arrayNotas = this.state.notas; // atribuo a essa variavel o array de notas
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas });
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <FormularioCadastro
+          categorias={this.state.categorias}
+          criarNota={this.criarNota.bind(this)}
+        />
         <main className="conteudo-principal">
-          <ListaDeCategorias 
-          adicionarCategoria={this.adicionarCategoria.bind(this)}
-          categorias={this.state.categorias} />
-          <ListaDeNotas 
+          <ListaDeCategorias
+            adicionarCategoria={this.adicionarCategoria.bind(this)}
+            categorias={this.state.categorias}
+          />
+          <ListaDeNotas
             apagarNota={this.deletarNota.bind(this)}
             notas={this.state.notas}
           />
