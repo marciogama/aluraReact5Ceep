fac52bbff9b6290c9d4a9683800dfa2e21520ebf
marciogama <marciogama2000@gmail.com>
2020-11-19 10:41:48 -0300

    adicionando categoria a uma nova nota

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -5,11 +5,17 @@ class FormularioCadastro extends Component {
     super(props);
     this.titulo = "";
     this.texto = "";
+    this.categoria = "Sem Categoria";
   }
 
   _handleMudancaTitulo(evento) {
     evento.stopPropagation();
     this.titulo = evento.target.value;
+  }
+
+  _handleMudancaCategoria(evento) {
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
   }
 
   _handleMudancaTexto(evento) {
@@ -20,13 +26,17 @@ class FormularioCadastro extends Component {
   _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
   }
 
   render() {
     return (
       <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
-        <select className="form-cadastro_input">
+        <select
+          onChange={this._handleMudancaCategoria.bind(this)}
+          className="form-cadastro_input"
+        >
+          <option defaultChecked={true}>Sem Categoria</option>
           {this.props.categorias.map((categoria) => {
             return <option>{categoria}</option>;
           })}
